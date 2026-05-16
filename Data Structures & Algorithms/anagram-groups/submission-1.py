b21a7aa56e4c4529aac5b0d169cc99d4ecc05209
Manas <67505485+manas1462005@users.noreply.github.com>
2026-05-16 12:10:49 +0530

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups={}
+        for word in strs:
+            freq=[0]*26
+            for ch in word:
+                freq[ord(ch)-ord('a')]+=1
+            key=tuple(freq)
+            if key not in groups:
+                groups[key]=[]
+            groups[key].append(word)
+        return list(groups.values())
